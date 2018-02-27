09fdba43d94fa4c61b6700b6cc5f4c88c37abf33
Zander <31848787+Zanalm@users.noreply.github.com>
2018-02-27 13:35:04 +0100

    Setting up for the questions in the quiz
    Adding functions in the script for handeling and displaying the questions

--- Website/myScript.js
+++ Website/myScript.js
@@ -7,3 +7,47 @@ function showTutorial() {
         x.style.display = "block";
     }
 }
+
+
+// This is for the quiz
+
+
+
+// This creates the div for the questions
+function createQuestionElement(index) {
+    var qElement = $('<div>', {
+      id: 'question'
+    });
+
+    var header = $('<h2>Question ' + (index + 1) + ':</h2>');
+    qElement.append(header);
+
+    var question = $('<p>').append(questions[index].question);
+    qElement.append(question);
+
+    var radioButtons = createRadios(index);
+    qElement.append(radioButtons);
+
+    return qElement;
+  }
+
+// Creates a list of the choices as radio inputs
+function createRadios(index) {
+  var radioList = $('<ul>');
+  var item;
+  var input = '';
+  for (var i = 0; i < questions[index].choices.length; i++) {
+    item = $('<li>');
+    input = '<input type="radio" name="answer" value=' + i + ' />';
+    input += questions[index].choices[i];
+    item.append(input);
+    radioList.append(item);
+  }
+  return radioList;
+}
+
+// handles the selected one and puts it in the array
+function choose() {
+  selections[questionCounter] = +$('input[name="answer"]:checked').val();
+}
+// End quiz
