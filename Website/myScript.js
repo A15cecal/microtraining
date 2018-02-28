2d702707e32f115fd65eae97efb200f823f18ef4
Zander <31848787+Zanalm@users.noreply.github.com>
2018-02-28 16:17:25 +0100

    fixed form
    Fixed the form to be more "material design"

--- Website/myScript.js
+++ Website/myScript.js
@@ -8,7 +8,45 @@ function showTutorial() {
   }
 }
 
+/*For the animations and siplay of the user-form*/
+$(window, document, undefined).ready(function() {
 
+  $('input').blur(function() {
+    var $this = $(this);
+    if ($this.val())
+      $this.addClass('used');
+    else
+      $this.removeClass('used');
+  });
+
+  var $ripples = $('.ripples');
+
+  $ripples.on('click.Ripples', function(e) {
+
+    var $this = $(this);
+    var $offset = $this.parent().offset();
+    var $circle = $this.find('.ripplesCircle');
+
+    var x = e.pageX - $offset.left;
+    var y = e.pageY - $offset.top;
+
+    $circle.css({
+      top: y + 'px',
+      left: x + 'px'
+    });
+
+    $this.addClass('is-active');
+
+  });
+
+  $ripples.on('animationend webkitAnimationEnd mozAnimationEnd oanimationend MSAnimationEnd', function(e) {
+  	$(this).removeClass('is-active');
+  });
+
+});
+/*end animations user-form*/
+
+// HUR FIXAR JAG DETTA MED TEXT ISTÄLLET FÖR SIFFROR??!!
 // This is for the quiz
 (function() {
   var questions = [{
@@ -85,10 +123,10 @@ function showTutorial() {
   });
 
   // Animates buttons on hover
-  $('.button').on('mouseenter', function () {
+  $('.quizBtn').on('mouseenter', function () {
     $(this).addClass('active');
   });
-  $('.button').on('mouseleave', function () {
+  $('.quizBtn').on('mouseleave', function () {
     $(this).removeClass('active');
   });
 
